dab1d6f071f386f0eb20a14c5132c0bd5d79fbc7
Kaïss Sarragossa <2466568@cegepmv.ca>
2025-01-30 11:01:41 -0500

    Test

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test{
-    public statci void main(String[] args){
-        Systeme.out.println("Test")
+    public static void main(String[] args){
+        System.out.println("Test");
     }
 }
